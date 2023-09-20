48ccce4636c5d5a7eb42f0c282fde55f46b32404
toho36 <toho0036@gmail.com>
2023-09-20 18:34:36 +0200

    Responsitivity for Main

--- components/Main.tsx
+++ components/Main.tsx
@@ -15,11 +15,12 @@ const Main = () => {
           background: 'linear-gradient(to right top, black, lightblue)',
           WebkitBackgroundClip: 'text',
           color: 'transparent',
+          padding: '0 20px 0  50px', // Add some padding for smaller screens
         }}
       >
         <Typography
           variant="h1"
-          fontSize={'100px'}
+          fontSize={{ xs: '40px', sm: '70px', md: '100px' }}
           fontWeight={'400'}
           sx={{
             maxWidth: '830px',
@@ -37,6 +38,7 @@ const Main = () => {
             justifyContent: 'center',
             alignItems: 'center',
             textAlign: 'center',
+            fontSize: { xs: '14px', sm: '16px', md: '18px' },
           }}
         >
           Elevate Your Webflow Projects with a Junior Front-End Web Developer's
